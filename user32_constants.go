261e50f8e6a64701eb82ec0d0174894e27ece4da
Simon Lapin <lapinsa42@gmail.com>
2023-10-19 08:53:30 +0300

    Adds user32 constants

---
+++ user32_constants.go
@@ -0,0 +1,15 @@
+package w32
+
+// https://docs.microsoft.com/en-us/windows/desktop/api/winuser/nf-winuser-enumdisplaymonitors
+const (
+	WINSTA_ALL_ACCESS        = 0x37F
+	WINSTA_ACCESSCLIPBOARD   = 0x0004
+	WINSTA_ACCESSGLOBALATOMS = 0x0020
+	WINSTA_CREATEDESKTOP     = 0x0008
+	WINSTA_ENUMDESKTOPS      = 0x0001
+	WINSTA_ENUMERATE         = 0x0100
+	WINSTA_EXITWINDOWS       = 0x0040
+	WINSTA_READATTRIBUTES    = 0x0002
+	WINSTA_READSCREEN        = 0x0200
+	WINSTA_WRITEATTRIBUTES   = 0x0010
+)
